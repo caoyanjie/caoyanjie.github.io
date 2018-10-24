3df8346c50e0380de5866f6fdc9b71d837791c43
caoyanjie <mister_wisdom@sina.com>
2018-10-24 15:52:43 +0800

    delete all files

--- code/crawler11/crawler11.py
+++
@@ -1,9 +0,0 @@
-# !/usr/bin/python3.4
-# -*- coding: utf-8 -*-
-
-import pytesseract
-from PIL import Image
-
-image = Image.open('../jpg/code.png')
-code = pytesseract.image_to_string(image)
-print(code)
